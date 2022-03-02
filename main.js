256e219b60ed4c03043812e3b2323ca63cda6062
nicolasrobles <nicolasrobles32@gmail.com>
2022-03-02 18:55:27 -0300

    cambio de document write a console log

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 //Uso de ciclos en conjunto con un array donde lista el nombre de los alumnos, 
 //su nota y si aprobaron superando la nota mínima de 6 o más.
 
-const ProyectosFinales = [10, 3, 4, 7, 1]
-const NombreDelAlumno = ["Carlitos", "Juancito", "Pepe", "Luciano", "Valentino"]
+const ProyectosFinales = [10, 3, 4, 7, 1];
+const NombreDelAlumno = ["Carlitos", "Juancito", "Pepe", "Luciano", "Valentino"];
 
 for (let i = 0; i <= 4; i++) {
-    document.write(NombreDelAlumno[i] + " sacó una nota de " + ProyectosFinales[i]);
+
     if (ProyectosFinales[i] >= 6) {
-        document.write(" y está aprobado")
+        console.log(NombreDelAlumno[i] + " sacó una nota de " + ProyectosFinales[i] + " y está aprobado");
     }
     else {
-        document.write(" y está está desaprobado")
+        console.log(NombreDelAlumno[i] + " sacó una nota de " + ProyectosFinales[i] + " y está desaprobado");
     }
-    document.write("<br>");
 }
